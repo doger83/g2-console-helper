06a80573a2f31daeb9ed1cac16218523428d49e5
Dominic Gerweck <dominic@gerweck2.de>
2021-06-07 20:12:38 +0200

    Add project files.

---
+++ ConsoleHelperLib/Imports.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleHelperLib
+{
+    public static partial class ConsoleUtils
+    {
+        #region kernel32.dll
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern IntPtr GetConsoleWindow();
+
+
+        #endregion
+
+    }
+}
